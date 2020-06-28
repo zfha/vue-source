b61952405630f719a2566e506482879406da300b
zfha <545227212@qq.com>
2020-06-28 20:10:18 +0800

    feat(full to compile to runtime): $mount

--- vue-2.6.11/src/platforms/web/entry-runtime-with-compiler.js
+++ vue-2.6.11/src/platforms/web/entry-runtime-with-compiler.js
@@ -1,101 +1,120 @@
 /* @flow */
 
-import config from 'core/config'
-import { warn, cached } from 'core/util/index'
-import { mark, measure } from 'core/util/perf'
+import config from "core/config";
+import { warn, cached } from "core/util/index";
+import { mark, measure } from "core/util/perf";
 
-import Vue from './runtime/index'
-import { query } from './util/index'
-import { compileToFunctions } from './compiler/index'
-import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
+import Vue from "./runtime/index";
+import { query } from "./util/index";
+import { compileToFunctions } from "./compiler/index";
+import {
+  shouldDecodeNewlines,
+  shouldDecodeNewlinesForHref,
+} from "./util/compat";
 
-const idToTemplate = cached(id => {
-  const el = query(id)
-  return el && el.innerHTML
-})
+// 带缓存功能的idTemplate
+const idToTemplate = cached((id) => {
+  const el = query(id);
+  return el && el.innerHTML;
+});
 
-const mount = Vue.prototype.$mount
+// 之前的mount，之前的mount方法不带模板编译功能
+const mount = Vue.prototype.$mount;
+
+// 准备在之前的mount里面增加新功能【带模板编译功能】，装饰器模式
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
-  el = el && query(el)
+  el = el && query(el);
 
   /* istanbul ignore if */
   if (el === document.body || el === document.documentElement) {
-    process.env.NODE_ENV !== 'production' && warn(
-      `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
-    )
-    return this
+    process.env.NODE_ENV !== "production" &&
+      warn(
+        `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
+      );
+    return this;
   }
 
-  const options = this.$options
+  const options = this.$options;
   // resolve template/el and convert to render function
+  /*处理模板templete，编译成render函数，render不存在的时候才会编译template，否则优先使用render*/
   if (!options.render) {
-    let template = options.template
+    let template = options.template;
+    /*template存在的时候取template，不存在的时候取el的outerHTML*/
     if (template) {
-      if (typeof template === 'string') {
-        if (template.charAt(0) === '#') {
-          template = idToTemplate(template)
+      if (typeof template === "string") {
+        if (template.charAt(0) === "#") {
+          template = idToTemplate(template);
           /* istanbul ignore if */
-          if (process.env.NODE_ENV !== 'production' && !template) {
+          if (process.env.NODE_ENV !== "production" && !template) {
             warn(
               `Template element not found or is empty: ${options.template}`,
               this
-            )
+            );
           }
         }
       } else if (template.nodeType) {
-        template = template.innerHTML
+        template = template.innerHTML;
       } else {
-        if (process.env.NODE_ENV !== 'production') {
-          warn('invalid template option:' + template, this)
+        if (process.env.NODE_ENV !== "production") {
+          warn("invalid template option:" + template, this);
         }
-        return this
+        return this;
       }
     } else if (el) {
-      template = getOuterHTML(el)
+      template = getOuterHTML(el);
     }
     if (template) {
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile");
       }
-
-      const { render, staticRenderFns } = compileToFunctions(template, {
-        outputSourceRange: process.env.NODE_ENV !== 'production',
-        shouldDecodeNewlines,
-        shouldDecodeNewlinesForHref,
-        delimiters: options.delimiters,
-        comments: options.comments
-      }, this)
-      options.render = render
-      options.staticRenderFns = staticRenderFns
+      /**
+       * 将template编译成render函数
+       * 这里会有render以及staticRenderFns两个返回，这是vue的编译时优化
+       * static静态不需要在VNode更新时进行patch，优化性能
+       */
+      const { render, staticRenderFns } = compileToFunctions(
+        template,
+        {
+          outputSourceRange: process.env.NODE_ENV !== "production",
+          shouldDecodeNewlines,
+          shouldDecodeNewlinesForHref,
+          delimiters: options.delimiters,
+          comments: options.comments,
+        },
+        this
+      );
+      options.render = render;
+      options.staticRenderFns = staticRenderFns;
 
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile end')
-        measure(`vue ${this._name} compile`, 'compile', 'compile end')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile end");
+        measure(`vue ${this._name} compile`, "compile", "compile end");
       }
     }
   }
-  return mount.call(this, el, hydrating)
-}
+  /*调用const mount = Vue.prototype.$mount保存下来的不带编译的mount*/
+  return mount.call(this, el, hydrating);
+};
 
 /**
  * Get outerHTML of elements, taking care
  * of SVG elements in IE as well.
  */
-function getOuterHTML (el: Element): string {
+function getOuterHTML(el: Element): string {
   if (el.outerHTML) {
-    return el.outerHTML
+    return el.outerHTML;
   } else {
-    const container = document.createElement('div')
-    container.appendChild(el.cloneNode(true))
-    return container.innerHTML
+    const container = document.createElement("div");
+    container.appendChild(el.cloneNode(true));
+    return container.innerHTML;
   }
 }
 
-Vue.compile = compileToFunctions
+Vue.compile = compileToFunctions;
 
-export default Vue
+export default Vue;
