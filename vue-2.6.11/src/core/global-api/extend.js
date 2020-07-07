d33cfa00b40eb72a9ed4e1508aa361b253e2d6d3
zfha <545227212@qq.com>
2020-07-07 15:33:02 +0800

    feat: 分析到instance vue

--- vue-2.6.11/src/core/global-api/extend.js
+++ vue-2.6.11/src/core/global-api/extend.js
@@ -1,95 +1,102 @@
 /* @flow */
 
-import { ASSET_TYPES } from 'shared/constants'
-import { defineComputed, proxy } from '../instance/state'
-import { extend, mergeOptions, validateComponentName } from '../util/index'
+import { ASSET_TYPES } from "shared/constants";
+import { defineComputed, proxy } from "../instance/state";
+import { extend, mergeOptions, validateComponentName } from "../util/index";
 
-export function initExtend (Vue: GlobalAPI) {
+export function initExtend(Vue: GlobalAPI) {
   /**
    * Each instance constructor, including Vue, has a unique
    * cid. This enables us to create wrapped "child
    * constructors" for prototypal inheritance and cache them.
    */
-  Vue.cid = 0
-  let cid = 1
+  /*
+    每个构造函数实例（包括Vue本身）都会有一个唯一的cid
+    它为我们能够创造继承创建自构造函数并进行缓存创造了可能
+  */
+  Vue.cid = 0;
+  let cid = 1;
 
   /**
    * Class inheritance
+   * 使用基础 Vue 构造器，创建一个“子类”。
+   * 其实就是扩展了基础构造器，形成了一个可复用的有指定选项功能的子构造器。
+   * 参数是一个包含组件option的对象。  https://cn.vuejs.org/v2/api/#Vue-extend-options
    */
   Vue.extend = function (extendOptions: Object): Function {
-    extendOptions = extendOptions || {}
-    const Super = this
-    const SuperId = Super.cid
-    const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {})
+    extendOptions = extendOptions || {};
+    /*父类的构造*/
+    const Super = this;
+    /*父类的cid*/
+    const SuperId = Super.cid;
+    const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {});
     if (cachedCtors[SuperId]) {
-      return cachedCtors[SuperId]
+      return cachedCtors[SuperId];
     }
 
-    const name = extendOptions.name || Super.options.name
-    if (process.env.NODE_ENV !== 'production' && name) {
-      validateComponentName(name)
+    const name = extendOptions.name || Super.options.name;
+    /*如果构造函数中已经存在了该cid，则代表已经extend过了，直接返回*/
+    if (process.env.NODE_ENV !== "production" && name) {
+      validateComponentName(name);
     }
 
-    const Sub = function VueComponent (options) {
-      this._init(options)
-    }
-    Sub.prototype = Object.create(Super.prototype)
-    Sub.prototype.constructor = Sub
-    Sub.cid = cid++
-    Sub.options = mergeOptions(
-      Super.options,
-      extendOptions
-    )
-    Sub['super'] = Super
+    const Sub = function VueComponent(options) {
+      this._init(options);
+    };
+    Sub.prototype = Object.create(Super.prototype);
+    Sub.prototype.constructor = Sub;
+    Sub.cid = cid++;
+    Sub.options = mergeOptions(Super.options, extendOptions);
+    Sub["super"] = Super;
 
     // For props and computed properties, we define the proxy getters on
     // the Vue instances at extension time, on the extended prototype. This
     // avoids Object.defineProperty calls for each instance created.
     if (Sub.options.props) {
-      initProps(Sub)
+      initProps(Sub);
     }
     if (Sub.options.computed) {
-      initComputed(Sub)
+      initComputed(Sub);
     }
 
     // allow further extension/mixin/plugin usage
-    Sub.extend = Super.extend
-    Sub.mixin = Super.mixin
-    Sub.use = Super.use
+    Sub.extend = Super.extend;
+    Sub.mixin = Super.mixin;
+    Sub.use = Super.use;
 
     // create asset registers, so extended classes
     // can have their private assets too.
     ASSET_TYPES.forEach(function (type) {
-      Sub[type] = Super[type]
-    })
+      Sub[type] = Super[type];
+    });
     // enable recursive self-lookup
     if (name) {
-      Sub.options.components[name] = Sub
+      Sub.options.components[name] = Sub;
     }
 
     // keep a reference to the super options at extension time.
     // later at instantiation we can check if Super's options have
     // been updated.
-    Sub.superOptions = Super.options
-    Sub.extendOptions = extendOptions
-    Sub.sealedOptions = extend({}, Sub.options)
+    Sub.superOptions = Super.options;
+    Sub.extendOptions = extendOptions;
+    Sub.sealedOptions = extend({}, Sub.options);
 
     // cache constructor
-    cachedCtors[SuperId] = Sub
-    return Sub
-  }
+    cachedCtors[SuperId] = Sub;
+    return Sub;
+  };
 }
 
-function initProps (Comp) {
-  const props = Comp.options.props
+function initProps(Comp) {
+  const props = Comp.options.props;
   for (const key in props) {
-    proxy(Comp.prototype, `_props`, key)
+    proxy(Comp.prototype, `_props`, key);
   }
 }
 
-function initComputed (Comp) {
-  const computed = Comp.options.computed
+function initComputed(Comp) {
+  const computed = Comp.options.computed;
   for (const key in computed) {
-    defineComputed(Comp.prototype, key, computed[key])
+    defineComputed(Comp.prototype, key, computed[key]);
   }
 }
--- vue-2.6.11/src/core/global-api/use.js
+++ vue-2.6.11/src/core/global-api/use.js
@@ -1,23 +1,26 @@
 /* @flow */
 
-import { toArray } from '../util/index'
+import { toArray } from "../util/index";
 
-export function initUse (Vue: GlobalAPI) {
+export function initUse(Vue: GlobalAPI) {
   Vue.use = function (plugin: Function | Object) {
-    const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    // 不需要重复添加
+    const installedPlugins =
+      this._installedPlugins || (this._installedPlugins = []);
     if (installedPlugins.indexOf(plugin) > -1) {
-      return this
+      return this;
     }
 
     // additional parameters
-    const args = toArray(arguments, 1)
-    args.unshift(this)
-    if (typeof plugin.install === 'function') {
-      plugin.install.apply(plugin, args)
-    } else if (typeof plugin === 'function') {
-      plugin.apply(null, args)
+    const args = toArray(arguments, 1);
+    args.unshift(this);
+    // 判断是否为函数，或者对象中install函数
+    if (typeof plugin.install === "function") {
+      plugin.install.apply(plugin, args);
+    } else if (typeof plugin === "function") {
+      plugin.apply(null, args);
     }
-    installedPlugins.push(plugin)
-    return this
-  }
+    installedPlugins.push(plugin);
+    return this;
+  };
 }
